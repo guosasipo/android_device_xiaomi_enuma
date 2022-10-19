b60a37a2449795a569af8acd61a61b44e2b4a113
guosasipo <leehojoonlee0820@gmail.com>
2022-10-19 11:24:10 +0900

    revert: nabu: Drop IMS

--- device.mk
+++ device.mk
@@ -152,6 +152,18 @@ PRODUCT_SOONG_NAMESPACES += \
 # 2-pane layout
 PRODUCT_PACKAGES += \
     androidx.window.extensions
+	
+# Telephony & IMS  
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
 
 # Update engine
 PRODUCT_PACKAGES += \
