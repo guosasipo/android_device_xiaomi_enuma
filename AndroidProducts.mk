53898b146b30a1ba517e0f613f330c9e2bea10c0
guosasipo <leehojoonlee0820@gmail.com>
2022-10-21 11:58:15 +0900

    enuma: PixelExperience Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2018-2021 aospOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_enuma.mk
+    $(LOCAL_DIR)/aosp_enuma.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_enuma-eng \
-    arrow_enuma-userdebug \
-    arrow_enuma-user
+    aosp_enuma-eng \
+    aosp_enuma-userdebug \
+    aosp_enuma-user
--- aosp_enuma.mk
+++ aosp_enuma.mk
@@ -11,8 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/enuma/device.mk)
 
-# Inherit common LineageOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common PixelExperience stuff
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_CHARACTERISTICS := tablet
 
@@ -20,11 +22,11 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := enuma
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2105K81C
-PRODUCT_NAME := arrow_enuma
+PRODUCT_NAME := aosp_enuma
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Xiaomi/enuma/enuma:11/RKQ1.200826.002/V13.0.6.0.RKZCNXM:user/release-keys
 
 # Gapps Build
-ARROW_GAPPS := true
+WITH_GAPPS := true
